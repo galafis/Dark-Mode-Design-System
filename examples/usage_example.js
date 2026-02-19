0b3385a24e8d14eed4f7ac3444c33ac947bdecc4
Gabriel Demetrios Lafis <33261275+galafis@users.noreply.github.com>
2026-02-19 16:35:43 -0300

    Aprimorar qualidade do projeto: package.json, exemplos de uso

---
+++ examples/usage_example.js
@@ -0,0 +1,23 @@
+/**
+ * Exemplo de uso - Dark-Mode-Design-System
+ *
+ * Demonstra as principais funcionalidades do projeto.
+ */
+
+// Importar modulos principais
+// const { updateToggleButton } = require('./src/script.js');
+// const { setTheme } = require('./src/script.js');
+// const { initThemeController } = require('./src/script.js');
+
+async function main() {
+  // Inicializar updateToggleButton
+  // const instance = new updateToggleButton();
+  console.log("updateToggleButton carregado com sucesso");
+
+  // Inicializar setTheme
+  // const instance = new setTheme();
+  console.log("setTheme carregado com sucesso");
+
+}
+
+main().catch(console.error);
